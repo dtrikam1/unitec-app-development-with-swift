0f5017d76a9610588af527ad9d39414cd2591db0
Dipesh Trikam <dipesht@datacom.co.nz>
2019-07-15 08:18:53 +1200

    Added lesson 5 project

---
+++ Lesson 5/Performance/Performance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Performance
+//
+//  Created by Dipesh Trikam on 14/07/19.
+//  Copyright © 2019 Dipesh Trikam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
