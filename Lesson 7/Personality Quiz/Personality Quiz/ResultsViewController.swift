7fec9185c1e3841860712dca8eecc094ea632d36
Dipesh Trikam <dipesht@datacom.co.nz>
2019-09-02 09:41:28 +1200

    Added questions screen

---
+++ Lesson 7/Personality Quiz/Personality Quiz/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  Personality Quiz
+//
+//  Created by Dipesh Trikam on 29/08/19.
+//  Copyright © 2019 Dipesh Trikam. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lesson 7/Personality Quiz/Personality Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Dipesh Trikam on 29/08/19.
+//  Copyright © 2019 Dipesh Trikam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
