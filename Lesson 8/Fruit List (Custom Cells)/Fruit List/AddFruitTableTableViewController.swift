111c85866a3573245463f7ca2f56d4eb75c1e5de
Hayden Do <HaydenD@datacom.co.nz>
2019-09-05 16:11:26 +1200

    Add lesson 8 project files

---
+++ Lesson 8/Fruit List (Custom Cells)/Fruit List/AddFruitTableTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  AddFruitTableTableViewController.swift
+//  Fruit List
+//
+//  Created by Hayden Do on 5/09/19.
+//  Copyright © 2019 Unitec. All rights reserved.
+//
+
+import UIKit
+
+class AddFruitTableTableViewController: UITableViewController {
+
+    @IBOutlet weak var fruitEmojiTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    var delegate: AddFruitDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    @IBAction func saveButtonPress(_ sender: UIBarButtonItem) {
+        guard let emoji = fruitEmojiTextField.text, let name = nameTextField.text, let description = descriptionTextField.text, let price = Double(priceTextField.text ?? "") else {
+            return
+        }
+        
+        let fruit = Fruit(emoji: emoji, name: name, description: description, price: price)
+        delegate?.addFruit(fruit: fruit)
+        
+        navigationController?.popViewController(animated: true)
+        
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 4
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
