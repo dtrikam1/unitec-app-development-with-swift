ee6cded79748613ec2f0dc7cc058aedc49c92282
Hayden Do <HaydenD@datacom.co.nz>
2019-10-22 09:30:47 +1300

    Add cocoapods project

--- Lesson 12/Cocoapods/Cocoapods/ViewController.swift
+++ Lesson 12/Cocoapods/Cocoapods/ViewController.swift
@@ -10,23 +10,119 @@ import UIKit
 import Alamofire
 import KeychainAccess
 import Lottie
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var animationViewWrapper: UIView!
+    @IBOutlet weak var textFieldA: UITextField!
+    @IBOutlet weak var textFieldB: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    let publishSubjectA = PublishSubject<String?>()
+    let publishSubjectB = PublishSubject<String?>()
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        alamofireExample()
-        keychainExample()
-        lottieExample()
-
+//        alamofireExample()
+//        keychainExample()
+//        lottieExample()
+        subjectExample()
+//        rxSwiftExample()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func rxSwiftExample() {
+        textFieldA.rx.text
+            .bind(to: publishSubjectA.asObserver())
+            .disposed(by: disposeBag)
+
+        textFieldB.rx.text
+            .bind(to: publishSubjectB.asObserver())
+            .disposed(by: disposeBag)
+        
+        publishSubjectA.asObservable()
+            .map({ textA in
+                return "Input is \(textA ?? "")"
+            })
+            .subscribe(onNext: { textA in
+                self.outputLabel.text = "\(textA)"
+            })
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(publishSubjectA.asObservable(), publishSubjectB.asObservable())
+            .throttle(2, scheduler: MainScheduler.instance)
+            .filter({ textA, textB in
+                guard let textA = textA, let textB = textB else {
+                    return true
+                }
+                return textA.count > 2 && textB.count > 2
+            })
+            .subscribe(onNext: { textA, textB in
+                self.outputLabel.text = "\(textA ?? ""), \(textB ?? "")"
+            })
+            .disposed(by: disposeBag)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+            self.publishSubjectA.asObserver().onNext("Manually triggered")
+        })
+    }
+    
+    private func subjectExample() {
+        let publishSubject = PublishSubject<String>()
+        
+        publishSubject.asObserver().onNext("blah")
+        
+        publishSubject.asObservable()
+            .subscribe(onNext: { value in
+                print(value)
+            })
+            .disposed(by: disposeBag)
+        
+        publishSubject.asObserver().onNext("blahblah")
+        publishSubject.asObserver().onNext("blahblahblah")
+
+        let behaviourSubject = BehaviorSubject<Int>(value: 0)
+        
+        behaviourSubject.asObserver().onNext(-1)
+        
+        behaviourSubject.asObservable()
+            .subscribe(onNext: { value in
+                print(value)
+            })
+            .disposed(by: disposeBag)
+        
+        behaviourSubject.asObserver().onNext(1)
+        behaviourSubject.asObserver().onNext(2)
+        behaviourSubject.asObserver().onNext(3)
+        
+        let replaySubject = ReplaySubject<String>.create(bufferSize: 2)
+        
+        replaySubject.asObservable()
+            .subscribe(onNext: { value in
+                print("Observer 1: \(value)")
+            })
+            .disposed(by: disposeBag)
+        
+        replaySubject.asObserver().onNext("strawberry")
+        replaySubject.asObserver().onNext("kiwi")
+        replaySubject.asObserver().onNext("melon")
+        
+        
+        replaySubject.asObservable()
+            .subscribe(onNext: { value in
+                print("Observer 2: \(value)")
+            })
+            .disposed(by: disposeBag)
     }
 
     private func alamofireExample() {
--- Lesson 12/Cocoapods/Pods/Target Support Files/Pods-Cocoapods/Pods-Cocoapods-frameworks.sh
+++ Lesson 12/Cocoapods/Pods/Target Support Files/Pods-Cocoapods/Pods-Cocoapods-frameworks.sh
@@ -163,11 +163,17 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainAccess/KeychainAccess.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainAccess/KeychainAccess.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
